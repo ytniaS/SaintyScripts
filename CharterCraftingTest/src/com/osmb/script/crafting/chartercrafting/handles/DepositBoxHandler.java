97ac9d49f72b774fe99f2d5fa33f0f2993e0499f
ytniaS <jacksainty@outlook.com>
2026-01-11 21:25:59 +0000

    Nightshade Gatherer release
    Nightshade Gatherer

--- CharterCraftingTest/src/com/osmb/script/crafting/chartercrafting/handles/DepositBoxHandler.java
+++ CharterCraftingTest/src/com/osmb/script/crafting/chartercrafting/handles/DepositBoxHandler.java
@@ -1,34 +1,35 @@
 package com.osmb.script.crafting.chartercrafting.handles;
 
+import java.util.Set;
+
 import com.osmb.api.ScriptCore;
 import com.osmb.api.item.ItemGroupResult;
 import com.osmb.api.item.ItemID;
 import com.osmb.api.scene.RSObject;
 
-import java.util.Collections;
-import java.util.Set;
-
 import static com.osmb.script.crafting.chartercrafting.State.depositing;
 
 public class DepositBoxHandler {
-	
 	private final ScriptCore core;
 	
 	public DepositBoxHandler(ScriptCore core) {
 		this.core = core;
 	}
+	
 	private static final int COINS = 995;
-	private static final Set<Integer> ITEM_IDS_TO_NOT_DEPOSIT = Set.of(COINS);
+	private static final Set<Integer> ITEM_IDS_TO_NOT_DEPOSIT = Set.of(
+			COINS,
+			ItemID.MOLTEN_GLASS,
+			ItemID.GLASSBLOWING_PIPE,
+			ItemID.ASTRAL_RUNE);
 	
 	public void handle() {
 		depositing = true;
-		
 		// 1) If deposit box UI is already open, handle it
 		if (core.getWidgetManager().getDepositBox().isVisible()) {
 			handleDepositInterface();
 			return;
 		}
-		
 		// 2) Find deposit box object in the scene
 		RSObject box = core.getObjectManager()
 				.getObject(o ->
@@ -37,18 +38,15 @@ public class DepositBoxHandler {
 								           o.canReach()
 				          )
 				.orElse(null);
-		
 		if (box == null) {
 			core.log(getClass(), "No deposit box in scene.");
 			return;
 		}
-		
 		// 3) Open deposit box
 		if (!box.interact("deposit")) {
 			core.log(getClass(), "Failed to interact with deposit box.");
 			return;
 		}
-		
 		// 4) Wait until the deposit box widget is visible
 		core.submitHumanTask(
 				() -> core.getWidgetManager().getDepositBox().isVisible(),
@@ -57,28 +55,22 @@ public class DepositBoxHandler {
 	}
 	
 	private void handleDepositInterface() {
-		
 		ItemGroupResult snapshot =
 				core.getWidgetManager()
 						.getDepositBox()
 						.search(ITEM_IDS_TO_NOT_DEPOSIT);
-		
 		if (snapshot == null) {
 			return;
 		}
-		
 		// Deposit everything EXCEPT coins (995)
 		boolean success = core.getWidgetManager()
 				.getDepositBox()
 				.depositAll(ITEM_IDS_TO_NOT_DEPOSIT);
-		
 		if (!success) {
 			core.log(getClass(), "Failed to deposit items.");
 			return;
 		}
-		
 		core.log(getClass(), "Deposited all items except coins.");
-		
 		// Close UI & release lock
 		core.getWidgetManager().getDepositBox().close();
 		depositing = false;
